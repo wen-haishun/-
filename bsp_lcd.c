f6a4fcacb1f4a77dc01f112c76284386026da74c
wen-haishun <120726961+wen-haishun@users.noreply.github.com>
2023-04-02 20:39:42 +0800

    Update bsp_lcd.c

--- bsp_lcd.c
+++ bsp_lcd.c
@@ -1,20 +1,25 @@
-#include "main.h"   // ��������ͷ�ļ�
-		     //�Լ�����ͷ�ļ�������"bsp_lcd.h" 
+#include "main.h"   // 需包含这个头文件
+		     //自己定义头文件，类似"bsp_lcd.h" 
 		     		 
 _Bool lcd_mode = 0; 
+u8 test_value = 10;
 void LCD_Proc(void)
-{
-	u8 test_value = 10;
-	u8 LCD_buf[20]; // ����һ�����飬20���ɣ���Ϊ��Ļ�ĳ�������20
-	//ֱ�������Ļ 
+{	
+	u8 LCD_buf[20]; // 定义一个数组，20即可，因为屏幕的长是20
+	//直接输出屏幕 
 	LCD_DisplayStringLine(Line0,(unsigned char*)"      LCD_TEST    "); 
-	sprintf((char*)display_buf," Value:%d",test_value); //ʹ��ƴ�Ӻ�������ʵ�ֱ������ַ�����ƴ�ӡ� 
-	LCD_DisplayStringLine(Line0,display_buf);//��ƴ�Ӻõ��ַ�����ӡ��
+	//使用拼接函数可以实现变量与字符串的拼接
+	sprintf((char*)display_buf," Value:%d",test_value); 
+	//将拼接好的字符串输出屏幕。
+	LCD_DisplayStringLine(Line0,display_buf);
 	
-	//���Ҫʵ����������,��һ����Ļ�е��ַ����͵ڶ�����Ļ�е���ʾ��ͬһ�У����ҵ�һ����Ļ�ĳ����ᵼ���л����ڶ�����Ļ��ʱ�����ʾ��һ����Ļ�ȵڶ�����Ļ������Ĳ���
+	//如果要实现切屏功能,第一个屏幕中的字符串和第二个屏幕中的显示在同一行，而且第一个屏幕的长，会导致切换到第二个屏幕的时候会显示第一个屏幕比第二个屏幕多出来的部分
 	/*
-		��ʽһ��ˢ����Ļ�����������������ʹ���������Լ���ʱ��ÿ�ζ�ˢ��Ӱ��۸С�
-		��ʽ��ʹ�ں������ӿո�ʹ֮����һ�¡� 
+		比如第一个屏幕 ABCDEDFGH 显示在第一行0开始,第二个屏幕 1234 显示在第一行0开始,由第一切换第二时,第二屏幕会显示1234EDFGH
+	*/
+	/*
+		方式一是刷新屏幕：但是如果遇到按键使变量自增自减的时候，每次都刷，影响观感。
+		方式二使在后面添加空格，使之长度一致。 
 	*/ 
 	if(lcd_mode == 0)
 	{
@@ -22,6 +27,6 @@ void LCD_Proc(void)
 	}
 	else
 	{
-		LCD_DisplayStringLine(Line0,(unsigned char*)"ABCD    "); //���ӿո� 
+		LCD_DisplayStringLine(Line0,(unsigned char*)"1234    "); //添加空格 
 	}
 }
