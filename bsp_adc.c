f0bb13c21e4e6d7da4511d2c6e9879bdfe2990dc
wen-haishun <120726961+wen-haishun@users.noreply.github.com>
2023-04-02 20:02:10 +0800

    Add files via upload

---
+++ bsp_adc.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+
+// ADCÖ´ÐÐ³ÌÐò
+u16 adc1_val,adc2_val;
+float volt_r37,volt_r38,volt_mcp;
+void ADC_Process(void)
+{
+
+	HAL_ADC_Start(&hadc1);
+	adc1_val = HAL_ADC_GetValue(&hadc1);
+	volt_r38 = adc1_val/4096.0f*3.3f;
+	
+    HAL_ADC_Start(&hadc2);
+	adc2_val = HAL_ADC_GetValue(&hadc2);
+	volt_r37 = adc2_val/4096.0f*3.3f;
+}
